c8676d7cc7952127544216d1fb92e68901cf6880
Ian Johnson <ijohnson@covermymeds.com>
2022-04-18 15:34:26 -0400

    Rename myprimal

--- myprimal/src/main.rs
+++ myprimal/src/main.rs
@@ -1,23 +1,23 @@
-mod myprime;
+mod myprimal;
 
 fn main() {
     let suspect = 5273;
-    println!("{} is prime: {}", suspect, myprime::sieve(suspect));
+    println!("{} is prime: {}", suspect, myprimal::sieve(suspect));
     let not_a_prime = 1024;
-    println!("{} is prime: {}", not_a_prime, myprime::sieve(not_a_prime));
+    println!("{} is prime: {}", not_a_prime, myprimal::sieve(not_a_prime));
 
     let n = 1000;
-    match myprime::nth(n) {
+    match myprimal::nth(n) {
         Some(number) => println!("{}th prime is {}", n, number),
         None => println!("I don't know anything about {}th prime.", n),
     }
     let nother = 100000;
-    match myprime::nth(nother) {
+    match myprimal::nth(nother) {
         Some(number) => println!("{}th prime is {}", nother, number),
         None => println!("I don't know anything about {}th prime.", nother),
     }
 
-    println!("{:?}", myprime::factor(2610));
+    println!("{:?}", myprimal::factor(2610));
 
-    println!("{:?}", myprime::count_divisors(2610));
+    println!("{:?}", myprimal::count_divisors(2610));
 }
